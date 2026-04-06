67e1cb574608de0a29b5974134f980f14adb9858
bitpunkxr <tony69elvis@gmail.com>
2026-04-07 00:14:26 +0100

    configured services and modified todo controller

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using AspNetCoreTodo.Services;
 
 namespace AspNetCoreTodo.Controllers
 {
     public class TodoController : Controller
     {
+        private readonly ITodoItemService _todoItemService;
+
+        public TodoController(ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
+
         public IActionResult Index()
         {
-            // Get to-do items from database
-            //put items into a model
-            //render using the model
+            var items = await _todoItemService.GetIncompleteItemAsync();
+
+            //..
         }
     }
 }
