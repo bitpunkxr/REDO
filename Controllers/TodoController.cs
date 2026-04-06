bb18830538b6748e9a7fcbdd0c3c0872614679dc
bitpunkxr <tony69elvis@gmail.com>
2026-04-07 00:59:26 +0100

    modified minor missing semicolons and wrong spelling

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AspNetCoreTodo.Services;
-using AspNetCoreTodo.Models
+using AspNetCoreTodo.Models;
 
 namespace AspNetCoreTodo.Controllers
 {
--- Startup.cs
+++ Startup.cs
@@ -1,12 +1,15 @@
-public void ConfigurationServices(IServiceCollection services)
+public class Startup
 {
-    // (...codes)
-    // The job of the ConfigureServices method is adding things to the service container, or the collection of services that ASP.NET Core knows about. The services.AddMvc line adds the services that the internal ASP.NET Core systems need
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // (...codes)
+        // The job of the ConfigureServices method is adding things to the service container, or the collection of services that ASP.NET Core knows about. The services.AddMvc line adds the services that the internal ASP.NET Core systems need
 
-    services.AddSingleton<ITodoItemService, FakeTodoItemService>();
+        services.AddSingleton<ITodoItemService, FakeTodoItemService>();
 
-    // This line tells ASP.NET Core to use the  FakeTodoItemService whenever the ITodoItemService interface is requested in a constructor or (anywhere else)
-    // AddSingleton adds your service to the service container as a singleton
+        // This line tells ASP.NET Core to use the  FakeTodoItemService whenever the ITodoItemService interface is requested in a constructor or (anywhere else)
+        // AddSingleton adds your service to the service container as a singleton
 
-    services.AddMvc();
+        services.AddMvc();
+    }
 }
